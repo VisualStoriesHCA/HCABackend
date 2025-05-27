a8cbe23d42f6d41664a6d779d5d3da7a23e5b0fc
mekadmini <M.Mekadmini@campus.lmu.de>
2025-05-27 23:47:52 +0200

    add first basic version

---
+++ app/routers/items.py
@@ -0,0 +1,67 @@
+# app/routers/items.py
+from typing import Optional, List, Dict
+
+from fastapi import APIRouter
+
+from ..models.session import get_user_session
+
+router = APIRouter(
+    prefix="/items",
+    tags=["items"],
+    responses={404: {"description": "Not found"}},
+)
+
+router = APIRouter()
+
+
+@router.get("/getUserInformation")
+async def get_user_information(
+        userID: str
+):
+    user = get_user_session(userID)
+    return user.to_dict()
+
+
+@router.get("/getUserStories")
+async def get_user_stories(
+        userID: str,
+        maxEntries: Optional[int] = 50,
+):
+    user = get_user_session(userID)
+    return {
+        "stories": [story.to_dict() for story in user.get_stories(maxEntries)]
+    }
+
+
+@router.get("/getStoryByID")
+async def get_story_by_id(
+        userID: str,
+        storyID: str
+):
+    user = get_user_session(userID)
+    return user.get_story(storyID).to_dict()
+
+
+@router.post("/updateImagesByText")
+async def update_images_by_text(
+        userID: str,
+        storyID: str,
+        updatedText: str,
+):
+    user = get_user_session(userID)
+    story = user.get_story(storyID)
+    story.set_text(updatedText)
+    story.update_images_by_text()
+    return story.to_dict()
+
+
+@router.post("/updateTextByImages")
+async def update_text_by_images(
+        userID: str,
+        storyID: str,
+        imageOperations: List[Dict],
+):
+    user = get_user_session(userID)
+    story = user.get_story(storyID)
+    story.update_from_image_operations(imageOperations)
+    return story.to_dict()
