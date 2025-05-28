5ea24cd1fab70589ad2cd311ce5d9b24c8c014af
mekadmini <M.Mekadmini@campus.lmu.de>
2025-05-28 20:42:21 +0200

    add create new user

--- app/models/session.py
+++ app/models/session.py
@@ -4,7 +4,8 @@ active_sessions = {}
 
 
 def create_user_session(user_id: str, user_name: str):
-    active_sessions[user_id] = User(user_id=user_id, username=user_name)
+    active_sessions[user_id] = User(user_id=user_id, username=user_name,)
+    return active_sessions[user_id]
 
 
 def get_user_session(user_id: str) -> User:
